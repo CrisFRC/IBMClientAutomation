7a2e59b0d7856e8593762cb270dd82417db35c52
Cristian Roa <cfroa@bancolombia.com.co>
2023-04-14 15:38:18 -0500

    Aplicado patron de diseño ScreenPlay

--- src/main/java/app/IbmAutomation.java
+++ src/main/java/app/IbmAutomation.java
@@ -1,6 +1,28 @@
 package app;
 
+
+
 public class IbmAutomation {
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 
 
--- src/main/java/controller/SystemIbmAccessClient.java
+++ src/main/java/controller/SystemIbmAccessClient.java
@@ -1,4 +1,16 @@
 package controller;
 
+import com.jacob.activeX.ActiveXComponent;
+
 public class SystemIbmAccessClient {
+    private static ActiveXComponent ibmSystem5250;
+
+    public static void putSystemIbmAccessClient(){
+        ibmSystem5250 = new ActiveXComponent("PCOMM.autECLSession");
+    }
+    public static ActiveXComponent getIbmSystem5250(){
+        return ibmSystem5250;
+    }
+
+
 }
